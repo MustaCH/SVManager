cad7ac25986677c7ebcba24d82dece224540a121
Ignacio Nicolás García Poletti <ip@discoverit.store>
2024-05-16 09:22:10 -0300

    database linked and deployed

--- app/containers/product-form-container/index.tsx
+++ app/containers/product-form-container/index.tsx
@@ -1,5 +1,12 @@
 import { connectDB } from "@/data";
 import Prod from "@/models/products";
+import {
+  Accordion,
+  AccordionItem,
+  Button,
+  Divider,
+  Input,
+} from "@nextui-org/react";
 
 async function getProducts() {
   await connectDB();
@@ -12,8 +19,26 @@ export default async function ProductFormContainer() {
   console.log(products);
 
   return (
-    <div>
-      <p></p>
-    </div>
+    <main className="flex flex-col gap-4 p-12">
+      <Input type="text" label="Nombre" />
+      <Divider />
+      <Input type="number" label="Precio" />
+      <Divider />
+      <div className="flex flex-col gap-4">
+        <label>Medidas:</label>
+        <div className="flex gap-4">
+          <Input type="text" label="Medida A" />
+          <Input type="text" label="Medida B" />
+          <Input type="text" label="Medida C" />
+          <Input type="text" label="Medida D" />
+        </div>
+      </div>
+      <Divider />
+      <Input type="text" label="Categoria" />
+      <Divider />
+      <Input type="file" label="Fotos" />
+      <Divider />
+      <Button color="primary">Cargar producto</Button>
+    </main>
   );
 }
