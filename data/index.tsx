6021cc23c3d854ab5d8108b358039348d3e117ca
Ignacio Nicolás García Poletti <ip@discoverit.store>
2024-05-16 07:51:41 -0300

    redeploying

--- data/index.tsx
+++ data/index.tsx
@@ -2,5 +2,5 @@ import mongoose from "mongoose";
 
 export async function connectDB() {
   const uri: string = process.env.MONGODB_URL as string;
-  await mongoose.connect(uri);
+  await mongoose.connect(uri, { bufferCommands: false });
 }
